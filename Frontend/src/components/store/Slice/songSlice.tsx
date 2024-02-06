8484f15194e81a764accaa249065e0e53a6c5713
Dagim Asnake <dagimasnake1@gmail>
2024-02-06 09:23:44 +0300

    make slice typescript

--- Frontend/src/components/store/Slice/songSlice.tsx
+++ Frontend/src/components/store/Slice/songSlice.tsx
@@ -1,33 +1,47 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-const initialSongState = { songs: [], genreSongs: [], selectedSong: null };
+interface Song {
+  _id: string;
+  title: string;
+  artist: string;
+  album: string;
+  genre: string;
+}
+
+interface SongState {
+  songs: Song[];
+  genreSongs: Song[];
+  selectedSong: Song | null;
+}
+
+const initialSongState: SongState = { songs: [], genreSongs: [], selectedSong: null };
 
 const songSlice = createSlice({
   name: 'song',
   initialState: initialSongState,
   reducers: {
-    addSong(state, action) {
+    addSong(state, action: PayloadAction<Song>) {
       const { title, artist, album, genre } = action.payload;
       const newSong = { _id: "1", title, artist, album, genre };
       state.songs.push(newSong);
     },
-    getAllSong(state, action) {
+    getAllSong(state, action: PayloadAction<Song[]>) {
       state.songs = action.payload;
     },
-    getGenreSong(state, action) {
+    getGenreSong(state, action: PayloadAction<Song[]>) {
       state.genreSongs = action.payload;
     },
-    getOneSong(state, action) {
+    getOneSong(state, action: PayloadAction<Song | null>) {
       state.selectedSong = action.payload;
     },
-    deleteSong(state, action) {
+    deleteSong(state, action: PayloadAction<string>) {
       const songId = action.payload;
       state.songs = state.songs.filter((song) => song._id !== songId);
       if (state.selectedSong && state.selectedSong._id === songId) {
         state.selectedSong = null;
       }
     },
-    updateSong(state, action) {
+    updateSong(state, action: PayloadAction<{ id: string, title: string, artist: string, album: string, genre: string }>) {
       const { id, title, artist, album, genre } = action.payload;
       const editSong = state.songs.find((song) => song._id === id);
       if (editSong) {
